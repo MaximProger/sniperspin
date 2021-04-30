0c8814ad11cd2067836892e3e5c24980d30ae72b
Maksim Koshman <maksimkoshman090@gmail.com>
2021-04-30 21:30:39 +0300

    Добавил шапку, интро и блок с предложениями

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,32 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  // Навигация
+  $(".nav__item").on("click", function (evt) {
+    evt.preventDefault();
+    $(this).toggleClass("nav__item--active");
+  });
+
+  // Бургер меню
+  $("#burgerBtn").click((evt) => {
+    evt.preventDefault();
+    $("#dropdownMenu").slideToggle();
+  });
+
+  // // Слайдер интро
+  $("#introSlider").not(".slick-initialized").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    centerMode: true,
+    autoplay: true,
+    arrows: false,
+    asNavFor: "#introNav",
+  });
+
+  // Навигация для слайдера
+  $("#introNav").not(".slick-initialized").slick({
+    arrows: false,
+    slidesToShow: 6,
+    asNavFor: "#introSlider",
+    focusOnSelect: true,
+  });
+});
