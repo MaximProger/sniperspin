bc3b75977439c3d124eaf881bd514d42593947f5
Maksim Koshman <maksimkoshman090@gmail.com>
2021-05-01 20:10:09 +0300

    Основные блоки десктопной версии готовы

--- app/js/app.js
+++ app/js/app.js
@@ -29,4 +29,59 @@ $(document).ready(function () {
     asNavFor: "#introSlider",
     focusOnSelect: true,
   });
+
+  // Слайдер ренды
+  $("#brandsSlider")
+    .not(".slick-initialized")
+    .slick({
+      slidesToShow: 7,
+      slidesToScroll: 1,
+      infinite: true,
+      nextArrow: $(".brands__arrow--next"),
+      prevArrow: $(".brands__arrow--prev"),
+    });
+
+  // Слайдер новости
+  $("#newsSlider")
+    .not(".slick-initialized")
+    .slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      infinite: true,
+      autoplay: false,
+      arrows: true,
+      nextArrow: $("#newsNext"),
+      prevArrow: $("#newsPrev"),
+      asNavFor: "#navNews",
+    });
+
+  // Навигация для слайдера
+  $("#navNews").not(".slick-initialized").slick({
+    arrows: false,
+    slidesToShow: 5,
+    asNavFor: "#newsSlider",
+    focusOnSelect: true,
+  });
+
+  // Слайдер блог
+  $("#blogSlider")
+    .not(".slick-initialized")
+    .slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      infinite: true,
+      autoplay: false,
+      arrows: true,
+      nextArrow: $("#blogNext"),
+      prevArrow: $("#blogPrev"),
+      asNavFor: "#navBlog",
+    });
+
+  // Навигация для слайдера
+  $("#navBlog").not(".slick-initialized").slick({
+    arrows: false,
+    slidesToShow: 5,
+    asNavFor: "#blogSlider",
+    focusOnSelect: true,
+  });
 });
